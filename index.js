08d0dd81961d0444988aa69e1a82a24ac635054e
Mordechai <mordwein77@gmail.com>
2022-10-06 18:10:53 -0400

    commit tree a35ec2f1130b13f666448a5b27d72e3bdf894afa

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random() * 2)
+
+const num3 = 16
+const num4 = 4
+const mod =  num3 / num4
+const max = Math.max(2,4,10,15,20)
+
+
+
+
